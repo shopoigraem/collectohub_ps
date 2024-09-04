e6ea45c9c9f149e5999fc2e0f80e9e854ca6e966
CollectoHub <collecttohub@gmail.com>
2024-09-04 10:39:10 +0100

    Added files and changes required for deployment

--- collectohub/core/settings.py
+++ collectohub/core/settings.py
@@ -132,12 +132,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
