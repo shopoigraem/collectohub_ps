499bd2dbf580ccb3579e6606824c4d3edd68a711
CollectoHub <collecttohub@gmail.com>
2024-09-06 15:00:50 +0100

    debug=False

--- collectohub/core/settings.py
+++ collectohub/core/settings.py
@@ -35,7 +35,7 @@ load_dotenv(env_path)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
